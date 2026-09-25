7f8cbb147a7cf3f17ab11838fb53ae0b03083be4
yangmuy5 <muyao.yang@mail.utoronto.ca>
2026-09-25 01:48:28 -0400

    Add psycopg connection pool

---
+++ backend/src/database/connection.py
@@ -0,0 +1,22 @@
+import os
+
+from psycopg.conninfo import make_conninfo
+from psycopg_pool import ConnectionPool
+
+def get_conn_info() -> str:
+
+    password = os.environ.get("POSTGRES_PASSWORD", "")
+    if not password:
+        raise ValueError("POSTGRES_PASSWORD is required")
+
+    return make_conninfo(
+        "",
+        host=os.environ.get("DB_HOST", "localhost"),
+        port=os.environ.get("DB_PORT", "5432"),
+        dbname=os.environ.get("POSTGRES_DB", "postgres"),
+        user=os.environ.get("POSTGRES_USER", "postgres"),
+        password=password,
+    )
+
+def create_pool() -> ConnectionPool:
+    return ConnectionPool(get_conn_info(), min_size=1, max_size=5, open=True)
