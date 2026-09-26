799a4a87c42ab1466dbcbb83cd57c89cc2d7fe01
yangmuy5 <muyao.yang@mail.utoronto.ca>
2026-09-26 01:13:42 -0400

    Add model for sessions

---
+++ backend/src/database/model/session.py
@@ -0,0 +1,15 @@
+from uuid import UUID
+from .message import Message
+
+class Session:
+
+    def __init__(self, session_id: UUID, messages: list[Message]) -> None:
+        self.session_id = session_id
+        self.messages = []
+
+    def add_message(self, message: Message) -> None:
+        self.messages.append(message)
+
+    def get_conversation_as_json(self):
+        pass
+        # TO DO once we figure out how a convo looks like
